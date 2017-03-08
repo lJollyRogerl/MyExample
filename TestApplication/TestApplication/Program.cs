fc6556beabab23344327192f68e3adf22985458e
lJollyRogerl <ya.dmitrievgleb@yandex.ru>
2017-03-08 22:39:43 +0500

    Read() => ReadLine()

--- TestApplication/TestApplication/Program.cs
+++ TestApplication/TestApplication/Program.cs
@@ -8,7 +8,7 @@ namespace TestApplication
         {
             Console.WriteLine("He, I'm here!");
             Console.Beep();
-            int i = int.Parse(Console.Read());
+            int i = int.Parse(Console.ReadLine());
             string message = "";
             if(i>20)
                 message = "Your int is bigger then 20";
