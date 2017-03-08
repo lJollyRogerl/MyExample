fe1bc7765c9448d0b416279a7ef8c39ef95a29af
lJollyRogerl <ya.dmitrievgleb@yandex.ru>
2017-03-08 22:38:04 +0500

    Update Program.cs

--- TestApplication/TestApplication/Program.cs
+++ TestApplication/TestApplication/Program.cs
@@ -8,6 +8,13 @@ namespace TestApplication
         {
             Console.WriteLine("He, I'm here!");
             Console.Beep();
+            int i = int.Parse(Console.Read());
+            string message = "";
+            if(i>20)
+                message = "Your int is bigger then 20";
+            else
+                message = "Your int is less then 20";
+            Console.WriteLine(message);
         }
     }
 }
