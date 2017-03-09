506f18149161a1e855a8da136262efeec9900c6b
lJollyRogerl <ya.dmitrievgleb@yandex.ru>
2017-03-09 21:48:48 +0500

    commit

---
+++ VPNMMapplication/VPNMMapplication/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using HtmlAgilityPack;
+
+namespace VPNMMapplication
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLoad_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                HtmlDocument html = new HtmlDocument();
+                html.LoadHtml(@"http://rfpl.org/tournaments/championship/tournament-table");
+                HtmlNode table = html.GetElementbyId("main-content");
+                foreach (var node in table.ChildNodes)
+                {
+                    MessageBox.Show(node.Attributes.ToString());
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.InnerException.Message);
+            }
+
+        }
+    }
+}
