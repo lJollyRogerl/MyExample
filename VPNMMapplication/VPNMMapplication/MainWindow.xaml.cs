14ea8ade48f30789bf99ddb002be167c56de241d
lJollyRogerl <ya.dmitrievgleb@yandex.ru>
2017-03-31 00:49:01 +0500

    ч

--- VPNMMapplication/VPNMMapplication/MainWindow.xaml.cs
+++ VPNMMapplication/VPNMMapplication/MainWindow.xaml.cs
@@ -193,5 +193,6 @@ namespace VPNMMapplication
             dispatcherTimer.Interval = TimeSpan.FromMinutes((int)e.NewValue);
         }
 
+
     }
 }
--- VPNMMapplication/VPNMMapplication/MainWindowMenu.cs
+++ VPNMMapplication/VPNMMapplication/MainWindowMenu.cs
@@ -1,10 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
 using System.Windows;
-
+using System.Windows.Controls;
+using System.Windows.Input;
+using Excel = Microsoft.Office.Interop.Excel;
 
 namespace VPNMMapplication
 {
@@ -25,24 +26,31 @@ namespace VPNMMapplication
 
         private void menuToExel_Click(object sender, RoutedEventArgs e)
         {
-            Microsoft.Office.Interop.Excel.Application ExcelApp = new Microsoft.Office.Interop.Excel.Application();
-            Microsoft.Office.Interop.Excel.Workbook ExcelWorkBook;
-            Microsoft.Office.Interop.Excel.Worksheet ExcelWorkSheet;
-            //Книга.
-            ExcelWorkBook = ExcelApp.Workbooks.Add(System.Reflection.Missing.Value);
-            //Таблица.
-            ExcelWorkSheet = (Microsoft.Office.Interop.Excel.Worksheet)ExcelWorkBook.Worksheets.get_Item(1);
-
-            //for (int i = 0; i < mM_MK_UnitDataGrid.get; i++)
-            //{
-            //    for (int j = 0; j < mM_MK_UnitDataGrid.Columns[1].r; j++)
-            //    {
-            //        ExcelApp.Cells[i + 1, j + 1] = mM_MK_UnitDataGrid.Rows[i].Cells[j].Value;
-            //    }
-            //}
-            //Вызываем нашу созданную эксельку.
-            ExcelApp.Visible = true;
-            ExcelApp.UserControl = true;
+            try
+            {
+                string pathToFile = "exportToExcel.сsv";
+                System.Windows.Forms.SaveFileDialog saveFile = new System.Windows.Forms.SaveFileDialog();
+                saveFile.Filter = "(*.csv) | *.csv";
+                if (saveFile.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    pathToFile = saveFile.FileName;
+                    DataGrid dg = mM_MK_UnitDataGrid;
+                    dg.SelectAllCells();
+                    dg.ClipboardCopyMode = DataGridClipboardCopyMode.IncludeHeader;
+                    ApplicationCommands.Copy.Execute(null, dg);
+                    dg.UnselectAllCells();
+                    String Clipboardresult = (string)Clipboard.GetData(DataFormats.CommaSeparatedValue);
+                    StreamWriter swObj = new StreamWriter(pathToFile);
+                    swObj.WriteLine(Clipboardresult);
+                    swObj.Close();
+                    Process.Start(pathToFile);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            
         }
 
         private void menuToXmlForRMS_Click(object sender, RoutedEventArgs e)
@@ -53,9 +61,10 @@ namespace VPNMMapplication
                 System.Windows.Forms.SaveFileDialog saveFile = new System.Windows.Forms.SaveFileDialog();
                 saveFile.Filter = "Файл импорта (*.xml) | *.xml";
                 if (saveFile.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
                     pathToFile = saveFile.FileName;
-                SaveToXML.BuildXmlDoc(fullCollection, pathToFile);
-
+                    SaveToXML.BuildXmlDoc(fullCollection, pathToFile);
+                }
             }
             catch (Exception ex)
             {
@@ -66,6 +75,16 @@ namespace VPNMMapplication
         private void menuClose_Click(object sender, RoutedEventArgs e)
         {
             this.Close();
+        }
+
+        private void helpDesription_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void helpWriteToDeveloper_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
